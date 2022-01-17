f09638e4cb3dccec500e41cf736d810764eabb68
Vitaly Turovsky <vital2580@icloud.com>
2022-01-17 21:21:42 +0300

    move createSelector within component

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,5 +1,5 @@
 import { createSelector } from '@reduxjs/toolkit'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { Post } from '../api'
@@ -8,24 +8,30 @@ import PostPreview from '../components/PostPreview'
 import { useGetPostsQuery } from '../redux/apiSlice'
 import { selectUserByUsername } from '../redux/users/slice'
 
-const selectPostsByUser = createSelector(
-    //
-    [(res: { data: Post[] }) => res.data, (res, userId: string) => userId],
-    (data, userId) => data.filter(({ author }) => author === userId),
-)
-
 const User: React.FC = () => {
     const { username } = useParams() as { username: string }
     const user = useSelector(state => selectUserByUsername(state as any, username))
+
+    const selectPostsByUser = useMemo(
+        () =>
+            createSelector(
+                //
+                [(data: Post[] | undefined) => data, (res, userId: string | undefined) => userId],
+                (data, userId) => data?.filter(({ author }) => author === userId),
+            ),
+        [],
+    )
+
     const { postsForUser } = useGetPostsQuery(undefined, {
-        skip: !!user,
         selectFromResult: result => ({
             ...result,
-            postsForUser: selectPostsByUser({ data: result.data! }, user!.id),
+            postsForUser: selectPostsByUser(result.data, user?.id),
         }),
     })
 
     if (!user) return <PageContent>User not found</PageContent>
+
+    if (!postsForUser) return <div>Loading</div>
 
     return (
         <PageContent>
